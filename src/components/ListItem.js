9e047b5555ca8a916aa0a013ce584ec396c844eb
Koray Can Yilmaz <krycnylmz@gmail.com>
2023-03-09 17:45:39 +0300

    fix: copy to clipboard

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,8 +3,35 @@ import React from 'react'
 function ListItem({value}) {
 
   const copyTextHandler = (emoji) => {
+    console.log(`panda ${emoji}`);
     navigator.clipboard.writeText(emoji);
   }
+
+  const unsecuredCopyToClipboard = (text) => { 
+    const textArea = document.createElement("textarea");
+    textArea.value=text;
+    document.body.append(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand('copy')
+    }
+    catch ( err ) {
+      console.error('Unable to copy to clipboard',err)}
+      document.body.removeChild(textArea)
+    };
+
+    const copyToClipboard = (content) => {
+      if (window.isSecureContext && navigator.clipboard) {
+        navigator.clipboard.writeText(content);
+      } else {
+        unsecuredCopyToClipboard(content);
+      }
+    };
+
+    const buttonPress = (emoji) => { copyToClipboard(emoji); 
+    console.log('Clipboard updated 📥\nNow try pasting!');
+  };
 
   return (
     <div className="bg-orange-100 p-4 rounded m-2 ">
@@ -17,7 +44,7 @@ function ListItem({value}) {
             {value.title}
           </h3>
         </div>
-        <button onClick={() => copyTextHandler(value.symbol)} className="bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:from-fuchsia-500 hover:to-violet-500 active:from-fuchsia-700  active:to-fuchsia-700 transition-all duration-300 ease-in-out active:bg-violet-700 text-white p-2 rounded">Click to copy emoji</button>
+        <button onClick={() => buttonPress(value.symbol)} className="bg-gradient-to-r from-violet-500 to-fuchsia-500 hover:from-fuchsia-500 hover:to-violet-500 active:from-fuchsia-700  active:to-fuchsia-700 transition-all duration-300 ease-in-out active:bg-violet-700 text-white p-2 rounded">Click to copy emoji</button>
       </div>
     </div>
   )
